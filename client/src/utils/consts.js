eb8cb55d77c10a079416e31c7c9613c692d1d056
trekvartista <isken_150502@mail.ru>
2022-04-01 03:50:34 +0600

    set API url port:5000

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -4,3 +4,5 @@ export const REGISTER_ROUTE = '/register'
 export const LOGIN_ROUTE = '/login'
 export const ITEM_ROUTE = '/item'
 export const BASKET_ROUTE = '/basket'
+
+export const API_URL = 'http://localhost:5000/'
