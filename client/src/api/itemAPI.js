88c56de7c17a9ca30032e6266384fee8537d326f
trekvartista <isken_150502@mail.ru>
2022-04-12 21:27:28 +0600

    implemented Brand and Item API

--- client/src/api/itemAPI.js
+++ client/src/api/itemAPI.js
@@ -10,3 +10,30 @@ export const fetchTypes = async () => {
 
     return data
 }
+
+export const createBrand = async (brand) => {
+    const { data } = await $authHost.post("api/brand", brand)
+
+    return data
+}
+export const fetchBrands = async () => {
+    const { data } = await $host.get("api/brand")
+
+    return data
+}
+
+export const createItem = async (item) => {
+    const { data } = await $authHost.post("api/item", item)
+
+    return data
+}
+export const fetchItems = async () => {
+    const { data } = await $host.get("api/item")
+
+    return data
+}
+export const fetchConcreteItem = async (id) => {
+    const { data } = await $host.get("api/item/" + id)
+
+    return data
+}
