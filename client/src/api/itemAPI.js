9e6c6d5e75328d7ba8e7d9d74672c3cad837446d
trekvartista <isken_150502@mail.ru>
2022-04-12 18:06:39 +0600

    implemented types API

---
+++ client/src/api/itemAPI.js
@@ -0,0 +1,12 @@
+import { $authHost, $host } from "./index";
+
+export const createType = async (type) => {
+    const { data } = await $authHost.post("api/type", type)
+
+    return data
+}
+export const fetchTypes = async () => {
+    const { data } = await $host.get("api/type")
+
+    return data
+}
