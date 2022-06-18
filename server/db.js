4f80889a6002962a9002835ed65672a1d30630aa
trekvartista <isken_150502@mail.ru>
2022-06-18 16:36:30 +0600

    experiments

--- server/db.js
+++ server/db.js
@@ -3,17 +3,10 @@ const { Sequelize } = require("sequelize");
 module.exports = new Sequelize(
     process.env.DB_NAME || "online_shop",
     process.env.DB_USER || "postgres",
-    process.env.DB_PASSWORD || "admin",
+    process.env.DB_PASSWORD || "root",
     {
         dialect: "postgres",
         host: process.env.DB_HOST || "localhost",
-        port: 8080,
-        pool: {
-            max: 100,
-            min: 0,
-            idle: 200000,
-            // @note https://github.com/sequelize/sequelize/issues/8133#issuecomment-359993057
-            acquire: 1000000,
-        },
+        port: process.env.DB_PORT || 5432
     }
 );
--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
+app.use('/.netlify/functions/server', router);
 
 // error handling has to be last
 app.use(errorHandler)
